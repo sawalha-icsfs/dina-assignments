d2159001926610c92d10d556a9bc4e1f57456b40
Dina.Alkhader <Dina.Alkhader@icsfs.com>
2023-02-16 09:41:09 +0300

    structuring the project

--- icsfs-training-java/ics-day2-practice/src/main/java/com/jkframework/training/day2/TrainingCourse.java
+++ icsfs-training-java/ics-day2-practice/src/main/java/com/jkframework/training/day2/TrainingCourse.java
@@ -2,27 +2,35 @@ package com.jkframework.training.day2;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Scanner;
 
 public class TrainingCourse {
 
-    public static final String TEXT_FILENAME = "..\\ics-training-java\\array.txt";
+    public static final String TEXT_FILENAME = "..\\..\\icsfs-training-java\\array.txt";
+    public static List<Integer> mostFreqNumList = new ArrayList<Integer>();
+    static Scanner scanner;
 
     public static void main(String args[]) throws IOException {
-        day2();
+        TrainingCourse course = new TrainingCourse();
+
+        course.inputArray();
 
     }
 
-    public static String day2() throws IOException {
-
+    public String inputArray() throws IOException {
         int n = 0;
         int sum = 0;
         double avg = 0;
         int maxcount = 0;
         int maxFreqElement1 = 0;
         int maxFreqElement2 = 0;
-        int maxFreqElement3 = 0;
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("number of array element in scanner: ");
@@ -46,7 +54,6 @@ public class TrainingCourse {
             int m = 0;
             int count = 0;
             int count1 = 0;
-            // int count2 = 0;
 
             for (j = 0; j < n; j++) {
                 if (array[c] == array[j]) {
@@ -59,11 +66,6 @@ public class TrainingCourse {
                     count1++;
                 }
             }
-            // for (m = 0; m < n; m++) {
-            // if (array[k] == array[m]) {
-            // count2++;
-            // }
-            // }
 
             if (count > maxcount) {
                 maxcount = count;
@@ -73,19 +75,13 @@ public class TrainingCourse {
                 maxcount = count1;
                 maxFreqElement2 = array[j];
             }
-            // if (count2 > maxcount) {
-            // maxcount = count2;
-            // maxFreqElement3 = array[m];
-            // }
 
         }
-        // duplicate(array);
-
+        // duplicateNumbers(array);
         System.out.println("Sum is : " + sum);
         System.out.println("Average is : " + avg);
         System.out.println("max Freq Element1 is : " + maxFreqElement1);
         System.out.println("max Freq Element2 is : " + maxFreqElement2);
-        System.out.println("max Freq Element3 is : " + maxFreqElement3);
 
         System.out.println("Array elements are in array.txt file ");
         for (int i = 0; i < n; i++) {
@@ -99,6 +95,53 @@ public class TrainingCourse {
             return TEXT_FILENAME;
         }
 
+        return null;
+    }
+
+    public List<Integer> duplicateNumbers(int[] array) {
+        int inputArray[] = array;
+        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+
+        int maxcount = 0;
+        int maxFreqElement1 = 0;
+
+        for (int i = 0; i < inputArray.length; i++) {
+            int count = 0;
+
+            for (int j = 0; j < inputArray.length; j++) {
+                if (inputArray[i] == inputArray[j]) {
+                    count++;
+                }
+            }
+            if (count > maxcount) {
+                maxcount = count;
+                maxFreqElement1 = array[i];
+                hashMap.put(maxFreqElement1, count);
+            }
+
+        }
+        List<Integer> FreqNumList = new ArrayList<Integer>();
+        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) {
+            FreqNumList.add(entry.getValue());
+        }
+        Collections.sort(FreqNumList);
+        mostFreqNumList.add(findNumber(hashMap, FreqNumList.get(FreqNumList.size() - 1)));
+        mostFreqNumList.add(findNumber(hashMap, FreqNumList.get(FreqNumList.size() - 2)));
+        mostFreqNumList.add(findNumber(hashMap, FreqNumList.get(FreqNumList.size() - 3)));
+        return mostFreqNumList;
+
+    }
+
+    public Integer findNumber(HashMap<Integer, Integer> map, int value) {
+        for (Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == value) {
+                if (mostFreqNumList.contains(entry.getKey())) {
+                    continue;
+                } else {
+                    return entry.getKey();
+                }
+            }
+        }
         return null;
     }
 
